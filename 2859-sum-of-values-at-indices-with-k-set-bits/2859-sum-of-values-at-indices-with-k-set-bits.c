31486f077e3e2ef350de3d36afaf88b3de722109
Rahul <144919406+TwoLobster@users.noreply.github.com>
2024-02-04 21:33:32 +0530

    Time: 3 ms (95.52%), Space: 5.9 MB (86.55%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.c
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.c
@@ -2,7 +2,14 @@ int sumIndicesWithKSetBits(int* nums, int numsSize, int k){
 int sum=0;
     for(int i=0; i<numsSize; i++)
     {
-        if(k==__builtin_popcount(i))
+        int count=0, temp;
+        temp=i;
+        while(temp)
+        {
+            count+=temp&1;
+         temp>>=1;
+        }
+        if(k==count)
            {
                sum+=nums[i];
            }
