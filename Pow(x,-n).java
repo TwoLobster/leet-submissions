27024022f12acac4ece5d572e65483834da02d3b
Rahul <144919406+TwoLobster@users.noreply.github.com>
2025-07-20 00:01:44 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 10.25%] - LeetPush

--- Pow(x,-n).java
+++ Pow(x,-n).java
@@ -1,7 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        // double madharchod= 
-        // System.out.print(madharchod);
-        return Math.pow(x, n);
+        if(n==0)return 1;
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return powPow(x, N);
+    }
+
+    public double powPow(double x, long n){
+        if(n==0)return 1;
+        double half=powPow(x, n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        else return half*half*x;
     }
 }
